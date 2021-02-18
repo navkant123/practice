c7f7b480de6a8c7897ae31d81efaa5aa810c94d2
Navkant Yadav <navkantraj1997@gmail.com>
2021-02-18 12:54:29 +0530

    I have added string logic

--- Logic.java
+++ Logic.java
@@ -1,1 +1,15 @@
 Here I will Write All Java Method logic.
+
+
+
+
+private static String[] finalString(String str1[], String str2[]) {
+		String result[] = new String[str1.length + str2.length];
+		for (int i = 0; i < str1.length; i++) {
+			result[i] = str1[i];
+		}
+		for (int j = 0; j < str2.length; j++) {
+			result[str1.length+j] = str2[j];
+		}
+		return result;
+	}
